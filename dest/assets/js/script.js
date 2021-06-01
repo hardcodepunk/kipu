69117983804144b57a3c407535fe634a74e054c4
Frieder Jan Moerman <howl@hardcodepunk.be>
2021-06-01 17:32:42 +0200

    finalizing responsive versions and safari line-height bug fix

--- dest/assets/js/script.js
+++ dest/assets/js/script.js
@@ -1,29 +1,32 @@
 var navItem = document.querySelectorAll('.js-nav-item');
 var body = document.getElementById('body');
 
-/* initiate leaflet map
-var mymap = L.map('map').setView([-33.429055, -70.621262], 17);
-var marker = L.marker([-33.429055, -70.621262]).addTo(mymap);
+//initiate leaflet map
+var mymap = L.map('map').setView([41.38674517083558, 2.161157001826284], 13);
+var marker = L.marker([41.38674517083558, 2.161157001826284]).addTo(mymap);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNqdWFjdWY3MDAxYmIzeWw0bjlzaW80cmUifQ.3HAer8K3D-7fu29uIPPfVA', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox.streets'
-}).addTo(mymap);*/
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNrcDg2ZXJkNzAzOHMycHQ3dWdtZmpvMWUifQ.aAsOVWi9LmL3adsY7k4MjA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNrcDg2ZXJkNzAzOHMycHQ3dWdtZmpvMWUifQ.aAsOVWi9LmL3adsY7k4MjA'
+}).addTo(mymap);
 
-/* open mobile menu when clicking on mobile nav icon
+console.log(document.getElementById('js-toggle-mobile-nav'));
+
+// open mobile menu when clicking on mobile nav icon
 document.getElementById('js-toggle-mobile-nav').onclick = function() {
   body.classList.toggle('open-menu');
-};*/
+}
 
 for (var i = 0; i < navItem.length; i++) {
   navItem[i].addEventListener('click', function(event) {
 
-    /*if (body.classList.contains('open-menu')) {
+    if (body.classList.contains('open-menu')) {
       body.classList.remove('open-menu');
-    }*/
+    }
 
     //get href of nav element and scroll to its section
     var idDestination = this.getAttribute("href").substring(1, this.length);
